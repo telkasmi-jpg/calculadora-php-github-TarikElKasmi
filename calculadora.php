ebf84a6451a49606ebbe220625fcfd9268647f27
Tarik <telkasmi@inscamidemar.cat>
2026-02-24 15:48:19 +0100

    Refactor: càlculs dins d'una funció

--- calculadora.php
+++ calculadora.php
@@ -3,20 +3,22 @@ $operacio = $_POST['op'];
 $numero1 = $_POST['n1'];
 $numero2 = $_POST['n2'];
 
-
-if ($op == "s") {
-    $res = $n1 + $n2;
-} else if ($op == "r") {
-    $res = $n1 - $n2;
-} else if ($op == "m") {
-    $res = $n1 * $n2;
-} else if ($op == "d") {
-    if ($n2 != 0) {
-        $res = $n1 / $n2;
+function calcular($operacio, $numero1, $numero2) {
+    if ($operacio == "s") {
+        return $numero1 + $numero2;
+    } else if ($operacio == "r") {
+        return $numero1 - $numero2;
+    } else if ($operacio == "d") {
+        if ($numero2 != 0) {
+            return $numero1 / $numero2;
+        }
+        return "Error: divisió per zero";
+    } else if ($operacio == "m") {
+        return $numero1 * $numero2;
     } else {
-        $res = "Error";
+        return "Error: operació desconeguda";
     }
 }
 
-echo $res;
+echo calcular($operacio, $numero1, $numero2);
 ?>
