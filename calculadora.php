0cf7d37ef210a0450351b1dbcb97920d86ea8a49
telkasmi-jpg <telkasmi@inscamidemar.cat>
2026-03-10 16:52:22 +0100

    Renombrades variables d'entrada i resultat
    Canvi: $op→$operacio, $n1→$primerNumero, $n2→$segonNumero, $res→$resultat

--- calculadora.php
+++ calculadora.php
@@ -1,35 +1,21 @@
 <?php
-
 $operacio = $_POST['op'];
-$numero1 = $_POST['n1'];
-$numero2 = $_POST['n2'];
+$primerNumero = $_POST['n1'];
+$segonNumero = $_POST['n2'];
 
-switch ($operacio) {
-    case "s":
-        $res = $numero1 + $numero2;
-        break;
-
-    case "r":
-        $res = $numero1 - $numero2;
-        break;
-
-    case "m":
-        $res = $numero1 * $numero2;
-        break;
-
-    case "d":
-        if ($numero2 != 0) {
-            $res = $numero1 / $numero2;
-        } else {
-            $res = "Error: no es pot dividir per zero";
-        }
-        break;
-
-    default:
-        $res = "Error: operació desconeguda";
-        break;
+if ($operacio == "s") {
+    $resultat = $primerNumero + $segonNumero;
+} else if ($operacio == "r") {
+    $resultat = $primerNumero - $segonNumero;
+} else if ($operacio == "m") {
+    $resultat = $primerNumero * $segonNumero;
+} else if ($operacio == "d") {
+    if ($segonNumero != 0) {
+        $resultat = $primerNumero / $segonNumero;
+    } else {
+        $resultat = "Error";
+    }
 }
 
-echo $res;
-
+echo $resultat;
 ?>
